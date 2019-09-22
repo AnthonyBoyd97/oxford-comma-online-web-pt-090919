7a2b46262123f276fbd69a7176906a80ef9b752b
Anthony N Boyd <anboyd97@gmail.com>
2019-09-22 03:35:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,9 @@ def oxford_comma(array)
   array.each_with_index do |item,index|
     if array[0]==array[-1]
       string="#{item}"
+    elsif array.length==2
+      string=array.join(", ")
+      return string
     elsif array[index]==array[-1]
       string+=", and #{item}"
     elsif index==0
