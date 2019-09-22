1187447e3db1062b74313f1681135135c7d49e1c
Anthony N Boyd <anboyd97@gmail.com>
2019-09-22 03:35:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
 def oxford_comma(array)
   string=""
   array.each_with_index do |item,index|
-    if array[0]==array[-1]
-      string="#{item}"
-    elsif array.length==2
+    if array.length==2
       string=array.join(", ")
       return string
+    elsif array[0]==array[-1]
+      string="#{item}"
     elsif array[index]==array[-1]
       string+=", and #{item}"
     elsif index==0
