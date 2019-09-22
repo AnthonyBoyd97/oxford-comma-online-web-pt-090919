bdbca8d6edac691ee406883a062a91f02a9dbbc5
Anthony N Boyd <anboyd97@gmail.com>
2019-09-22 03:27:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  string=""
+  array.each_with_index do |item,index|
+    if array[index]==array[-1]
+      string+=", and #{item}"
+    elsif index==0
+      string+="#{item}"
+    else
+      string+=", #{item}"
+    end
+  end
 end
